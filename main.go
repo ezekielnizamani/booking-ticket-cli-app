76b207ea20e2bba4f2f5417f0439dd58579f2d20
ezekiel <ezekielnizamani0@gmail.com>
2023-10-04 23:17:31 +0500

    created variables

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Print("hello world");
+func main() {
+	var conferenceName = "Go conference"
+	const totalTickets = 50
+	var remainingTickets = 50
+	fmt.Println("Welcome to our", conferenceName, "application")
+	fmt.Println("we have total ", totalTickets, "and ", remainingTickets, "are remaining")
+	fmt.Println("Get your tickets to attend")
 }
