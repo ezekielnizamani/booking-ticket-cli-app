47464426ba4e805cfd1f9883438b182eecd5401b
ezekiel <ezekielnizamani0@gmail.com>
2023-10-06 00:06:00 +0500

    got input fro user using fmt.scan

--- main.go
+++ main.go
@@ -6,7 +6,21 @@ func main() {
 	var conferenceName = "Go conference"
 	const totalTickets = 50
 	var remainingTickets = 50
-	fmt.Println("Welcome to our", conferenceName, "application")
-	fmt.Println("we have total ", totalTickets, "and ", remainingTickets, "are remaining")
+	fmt.Printf("Welcome to our %v application\n", conferenceName)
+	fmt.Printf("we have total %v and %v are remaining\n", totalTickets, remainingTickets)
 	fmt.Println("Get your tickets to attend")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+	fmt.Println("Please write your first name")
+	fmt.Scan(&firstName)
+	fmt.Println("Please write your Last name")
+	fmt.Scan(&lastName)
+	fmt.Println("Please write your email")
+	fmt.Scan(&email)
+	fmt.Println("Please tell us how many tickets you want")
+	fmt.Scan(&userTickets)
+	fmt.Printf("first name %v last name %v email %v and how many user want to buy tickets %v", firstName, lastName, email, userTickets)
 }
